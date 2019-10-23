4600902a5e0e6a86584867c9733f6a5c894a6f4b
Remon Nashid <remon.sherin@gmail.com>
2019-10-22 20:07:58 -0700

    Updates

---
+++ precache-manifest.07c7279b293ea68a21a73bee77d9e1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b076fb82052906afa92be313b42b9c",
+    "url": "/permissions/index.html"
+  },
+  {
+    "revision": "2393005a042ea7e761a9",
+    "url": "/permissions/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "87083811d0aada4fc7f8",
+    "url": "/permissions/static/css/main.3b9fece1.chunk.css"
+  },
+  {
+    "revision": "2393005a042ea7e761a9",
+    "url": "/permissions/static/js/2.fa175986.chunk.js"
+  },
+  {
+    "revision": "87083811d0aada4fc7f8",
+    "url": "/permissions/static/js/main.0f855116.chunk.js"
+  },
+  {
+    "revision": "6978f46c0714492870be",
+    "url": "/permissions/static/js/runtime~main.cebe21d2.js"
+  }
+]);
